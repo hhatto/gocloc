2081063fb3fc552ad080f17d17ce935e7ed4f737
Albert Nigmatzianov <albertnigma@gmail.com>
2017-05-24 23:28:25 +0200

    Fix typos in Options

--- option.go
+++ option.go
@@ -3,7 +3,7 @@ package main
 import "regexp"
 
 type Options struct {
-	Byfile         bool   `long:"by-file" description:"report results for every source file encountered."`
+	Byfile         bool   `long:"by-file" description:"report results for every encountered source file"`
 	SortTag        string `long:"sort" default:"code" description:"sort based on a certain column"`
 	OutputType     string `long:"output-type" default:"default" description:"output type [values: default,cloc-xml,sloccount]"`
 	ExcludeExt     string `long:"exclude-ext" description:"exclude file name extensions (separated commas)"`
@@ -11,7 +11,7 @@ type Options struct {
 	MatchDir       string `long:"match-d" description:"include dir name (regex)"`
 	NotMatchDir    string `long:"not-match-d" description:"exclude dir name (regex)"`
 	Debug          bool   `long:"debug" description:"dump debug log for developer"`
-	SkipUniqueness bool   `long:"skip-uniqueness" description:"skip uniqueness files"`
+	SkipDuplicated bool   `long:"skip-duplicated" description:"skip duplicated files"`
 	ShowLang       bool   `long:"show-lang" description:"print about all languages and extensions"`
 }
 
