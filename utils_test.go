25e20c7ea72bb7df3c49fb6170ded755d53e548f
Hideo Hattori <hattori-h@klab.com>
2017-05-30 01:54:00 +0900

    add test fo containComments

--- utils_test.go
+++ utils_test.go
@@ -2,6 +2,20 @@ package main
 
 import "testing"
 
+func TestContainComments(t *testing.T) {
+	line := "/* hoge */"
+	st := "/*"
+	ed := "*/"
+	if containComments(line, st, ed) {
+		t.Errorf("invalid")
+	}
+
+	line = "/* comment"
+	if !containComments(line, st, ed) {
+		t.Errorf("invalid")
+	}
+}
+
 func TestCheckMD5SumIgnore(t *testing.T) {
 	fileCache = make(map[string]struct{})
 
