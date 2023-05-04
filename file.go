a88edc52b3eea697687f9546f6ac74a03c91c5fb
Hideo Hattori <hhatto.jp@gmail.com>
2023-05-04 23:34:12 +0900

    fix: SA6002

--- file.go
+++ file.go
@@ -62,7 +62,7 @@ func AnalyzeReader(filename string, language *Language, file io.Reader, opts *Cl
 	buf := getByteSlice()
 	defer putByteSlice(buf)
 	scanner := bufio.NewScanner(file)
-	scanner.Buffer(buf, 1024*1024)
+	scanner.Buffer(buf.Bytes(), 1024*1024)
 
 scannerloop:
 	for scanner.Scan() {
